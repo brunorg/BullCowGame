160e5d7affce8fe0a9057f3737e026eadf972e7d
Bruno Roberto Gonçalves <goncalvesb@gmail.com>
2018-07-22 22:24:31 -0300

    Variables and cin for for input

--- main.cpp
+++ main.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    constexpr int WORLD_LENGTH = 5;
+
+    // introduce the game
+    constexpr int WORLD_LENGTH = 9;
 
     cout << "Welcome to Bulls and Cows, a fun word game" << endl;
     cout << "Can you guess the " << WORLD_LENGTH;
     cout << " letter isogram I'm thinking of?" << endl;
+    cout << endl;
 
+    // get a guess from the player
+    cout << "Enter you guess: ";
+    string Guess = "";
+    cin >> Guess;
+
+    // repeat the guess back to them
+    cout << "Your guess was: " << Guess << endl;
+
+    // get a guess from the player
+    cout << "Enter you guess: ";
+    cin >> Guess;
+
+    // repeat the guess back to them
+    cout << "Your guess was: " << Guess << endl;
+
+    cout << endl;
     return 0;
 }
