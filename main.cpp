a4c20fe6fa01032d003c9ded3386861e8f955552
Bruno Roberto Gonçalves <goncalvesb@gmail.com>
2018-07-23 00:02:25 -0300

    Booleans and comparisons & Using do and while in C++

--- main.cpp
+++ main.cpp
@@ -6,13 +6,28 @@ using namespace std;
 void PrintIntro();
 void PlayGame();
 string GetGuess();
+bool AskToPlayAgain();
 
 // the entry point for our application
 int main()
 {
-    PrintIntro();
-    PlayGame();
+    bool bPlayAgain;
+    do
+    {
+        PrintIntro();
+        PlayGame();
+        bPlayAgain = AskToPlayAgain();
+    } while (bPlayAgain);
+
     return 0; // exit the application
+}
+
+bool AskToPlayAgain()
+{
+    cout << "Do you want to play again? ";
+    string Response = "";
+    getline(cin, Response);
+    return (Response[0] == 'y' || Response[0] == 'Y');
 }
 
 void PlayGame()
